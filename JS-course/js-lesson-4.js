74cbd62e8e82a65e0b0ce0eb96a1499d91197579
Kuznetsov_Y <yusergeevi4@gmail.com>
2020-04-30 20:51:19 +0300

    rewrite code with task

--- JS-course/js-lesson-4.js
+++ JS-course/js-lesson-4.js
@@ -3,59 +3,47 @@
 /*          Task #1         */
 console.log("Task#1");
 
-var number = {
-    hundreds: undefined,
-    tens: undefined,
-    digits: undefined,
+function fillNumber(num) {
+    // Проверки на текс и соответсвующие условия + обнуление данных
 
-    fillNumber: function (num) {
-        // Проверки на текс и соответсвующие условия + обнуление данных
-
-        if (isNaN(parseInt(num))) {
-            console.log("'" + num + "'" + " is not a number");
-            return false;
-        }
-
-        if (num < 0) console.log("Number < 0. The number will be converted to positive");
-
-        if (num > 999) {
-            console.log("Number > 1000");
-            return false;
-        }
-
-        num = Math.abs(num).toString().split("");
-
-        if (num.length == 3) this.hundreds = num[num.length - 3];
-        if (num.length >= 2) this.tens = num[num.length - 2];
-        this.digits = num[num.length - 1];
-
-        return true;
-    },
-
-    showNumber: function () {
-        console.log("Number:");
-        if (this.hundreds) console.log("Hundreds:" + this.hundreds);
-        if (this.tens) console.log("Tens:" + this.tens);
-        console.log("Digits:" + this.digits);
-    },
-
-    resetNumber: function () {
-        this.hundreds = undefined;
-        this.tens = undefined;
-        this.digits = undefined;
+    if (isNaN(parseInt(num))) {
+        console.log("'" + num + "'" + " is not a number");
+        return number;
     }
+
+    if (num < 0) console.log("Number < 0. The number will be converted to positive");
+
+    if (num > 999) {
+        console.log("Number > 1000");
+        return number;
+    }
+
+    num = Math.abs(num).toString();
+
+    if (num.length == 3) number.hundreds = num[num.length - 3];
+    if (num.length >= 2) number.tens = num[num.length - 2];
+    number.digits = num[num.length - 1];
+
+    return number;
 }
 
-number.resetNumber();
-var yourNum = prompt("Enter a number < 1000", 100);
+function showNumber() {
+    if (number.hundreds) console.log("Hundreds:" + number.hundreds);
+    if (number.tens) console.log("Tens:" + number.tens);
+    if (number.digits) console.log("Digits:" + number.digits);
+
+    console.log(number);
+}
+var number = {};
+
+fillNumber(prompt("Enter a number again", 100));
 
 while (true) {
-
-    if (!number.fillNumber(yourNum)) {
-        yourNum = prompt("Enter a number again", 100);
+    if (number.digits === undefined) {
+        fillNumber(prompt("Enter a number again", 100));
         continue;
     }
 
-    number.showNumber();
+    showNumber();
     break;
 }
